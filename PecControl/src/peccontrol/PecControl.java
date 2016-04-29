fa78e439f49fcdf26f1e83d325136e5a0782e273
02450821236 <02450821236@JIPA-LABIN05-07.ifro.local>
2016-04-29 10:33:06 -0400

    ola testando djajFHIE

--- PecControl/src/peccontrol/PecControl.java
+++ PecControl/src/peccontrol/PecControl.java
@@ -5,7 +5,7 @@ public class PecControl {
     
     private int oi;
     private int ola; 
-    //coment
+    //comentjxnjxnjcndjnvjdfnvbjkld
     public static void main(String[] args) {
         // TODO code application logic here
     }
