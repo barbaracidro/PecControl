1c83c5c4bc1e5a8cae066a86e40dd72008daaf17
03349151205 <03349151205@JIPA-LABIN05-08.ifro.local>
2016-05-20 08:51:26 -0400

    teste

--- PecControl/src/Codigos/MenuInicial.java
+++ PecControl/src/Codigos/MenuInicial.java
@@ -4,8 +4,7 @@ package Codigos;
 import Telas.FormMenuInicial;
 
 public class MenuInicial {
-       FormMenuInicial form = new FormMenuInicial();
-       form.setLocationRelativeTo(form);
-       form.setVisible(true);
+       
+       //comenta
     
 }
